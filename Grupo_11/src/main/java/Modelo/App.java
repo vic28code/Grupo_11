b2fd94219a8741b1faa66820d76b23cf710c426a
Vic <hvangulo@espol.edu.ec>
2024-08-03 13:42:35 -0500

    Ligeras correciones, txts, fxml

--- Grupo_11/src/main/java/Modelo/App.java
+++ Grupo_11/src/main/java/Modelo/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("programa"), 720, 550);
+        scene = new Scene(loadFXML("programa"));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
